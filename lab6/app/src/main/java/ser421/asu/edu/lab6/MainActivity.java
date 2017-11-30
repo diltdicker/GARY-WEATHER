8159268b62514120afac2eeabd56bd7bca4e015f
diltdicker <diltdicker@gmail.com>
2017-11-29 17:07:37 -0700

    lab2 inprogress please note the class name changes

---
+++ lab6/app/src/main/java/ser421/asu/edu/lab6/MainActivity.java
@@ -0,0 +1,120 @@
+package ser421.asu.edu.lab6;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.ValueCallback;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
+import org.json.*;
+
+import java.util.Arrays;
+
+
+/*
+    Note Weird Bug found when you tilt the phone on its side: it resets the app
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private WebView firstview;
+    private Button buttonChange;
+    private Button buttonUpdateWeather;
+    static String current;
+    static String selectionChoice="FINISH";
+    static JSONArray selectedCities;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonChange = (Button) findViewById(R.id.buttonChange);
+        buttonUpdateWeather = (Button) findViewById(R.id.buttonShowWeather);
+        firstview = (WebView) findViewById(R.id.webView);
+        firstview.setWebContentsDebuggingEnabled(true);
+        WebSettings webSettings = firstview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        firstview.loadUrl(getString(R.string.choiceURL));
+        firstview.addJavascriptInterface(new JavaScriptInterface(this.getApplicationContext()), "Android");
+        selectionChoice = "NewYork";
+        buttonChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //validates changes and sends them to second activity
+                //Intent WeatherIntent = new Intent(MainActivity.this, WeatherActivity.class);
+                //WeatherIntent.putExtra("selection", selectionChoice);
+                //startActivity(WeatherIntent);
+                firstview.evaluateJavascript("grabSelected();", new ValueCallback<String>() {
+
+                    @Override
+                    public void onReceiveValue(String citiesJSON) {
+                        citiesJSON = citiesJSON.substring(1, citiesJSON.length()-1);
+                        String[] list = citiesJSON.split(",");
+                        if (list[0].compareTo("") == 0) {
+                            // empty string
+                        } else  {
+                            selectionChoice = "Phoenix";
+                        }
+                        Log.d("LogName","Dallas = " + list[0]);
+
+                    }
+                });
+                Log.d("LogName", "Race condition Main");
+            }
+        });
+
+        buttonUpdateWeather.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // open second activity webview for user
+
+                Intent WeatherIntent = new Intent(MainActivity.this, WeatherActivity.class);
+                startActivityForResult(WeatherIntent, 1);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int mainCode, int returnCode, Intent intent) {
+        super.onActivityResult(mainCode, returnCode, intent);
+        if (mainCode == 1) {
+            if(returnCode == RESULT_OK) {
+                String returnText = intent.getStringExtra("textToMain");
+                // Logs data that came from second activity (after you press the back button)
+                Log.d("LogName", returnText);
+                Log.d("LogName", selectionChoice);
+                // the important thing we need to get here is the selected city on the second activity
+            }
+        }
+    }
+}
+
+class JavaScriptInterface {
+    private Context mainContext;
+    private JSONArray citiesList;
+
+    JavaScriptInterface(Context context) {
+        mainContext = context;
+    }
+
+    @JavascriptInterface
+    public void test(String text) {
+        Log.d("LogName", text);
+    }
+
+    public void updateCities(String citiesJSON) {
+        try {
+            JSONObject cityObj = new JSONObject(citiesJSON);
+            MainActivity.selectedCities = cityObj.getJSONArray("cities");
+
+        } catch (org.json.JSONException e) {
+            Log.d("LogName", "JSON Error");
+        }
+    }
+}
---
+++ lab6/app/src/main/java/ser421/asu/edu/lab6/WeatherActivity.java
@@ -0,0 +1,57 @@
+package ser421.asu.edu.lab6;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * Created by diltd on 11/27/2017.
+ */
+
+public class WeatherActivity extends AppCompatActivity {
+
+    private WebView secondView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceSate) {
+        super.onCreate(savedInstanceSate);
+        setContentView(R.layout.activity_weather);
+
+        String name = getIntent().getStringExtra("selection");
+
+        secondView = (WebView) findViewById(R.id.weatherView);
+        secondView.setWebViewClient(new WebViewClient());
+        secondView.setWebChromeClient(new WebChromeClient());
+        secondView.setWebContentsDebuggingEnabled(true);
+        WebSettings webSettings = secondView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setAllowUniversalAccessFromFileURLs(true);
+        secondView.loadUrl(getString(R.string.weather2URL));
+        secondView.addJavascriptInterface(new JavaScriptInterface(this.getApplicationContext()), "Android");
+        Intent intent = new Intent();
+        // These last 2 are to send values back to the original activity
+        intent.putExtra("textToMain", "value_here");
+        setResult(RESULT_OK, intent);
+
+        Log.d("LogName", "Race condition Weather");
+        if (name != null) {
+            // closes this activity out before it shows up on the user's screen
+            // evaluate JS
+
+            finish();
+        }
+    }
+
+    protected void insertCityValues (WebView view, String JSONList) {
+
+    }
+}
